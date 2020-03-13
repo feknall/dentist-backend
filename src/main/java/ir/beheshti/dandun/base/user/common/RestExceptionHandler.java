ab32bb576edb06d8f47bfd39086e505cb20ffb08
hamid <h.fazli.k@gmail.com>
2020-03-14 00:32:11 +0330

    convert to internal

--- src/main/java/ir/beheshti/dandun/base/user/common/RestExceptionHandler.java
+++ src/main/java/ir/beheshti/dandun/base/user/common/RestExceptionHandler.java
@@ -39,7 +39,7 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(UserException.class)
     protected ResponseEntity<Object> handleInternalServerError(UserException ex) {
-        ApiError apiError = new ApiError(HttpStatus.OK);
+        ApiError apiError = new ApiError(HttpStatus.INTERNAL_SERVER_ERROR);
         apiError.setCode(ex.getCode());
         apiError.setMessage(ex.getMessage());
         return buildResponseEntity(apiError);
