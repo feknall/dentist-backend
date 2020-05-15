03d6f64deb2e39caee0269ed7bc1640640d9b286
hamid <h.fazli.k@gmail.com>
2020-05-15 14:27:34 +0430

    minor fix

--- src/main/java/ir/beheshti/dandun/base/socket/ChatOutputDto.java
+++ src/main/java/ir/beheshti/dandun/base/socket/ChatOutputDto.java
@@ -1,29 +1,21 @@
 package ir.beheshti.dandun.base.socket;
 
-import ir.beheshti.dandun.base.user.dto.socket.MessageOutputDto;
 import ir.beheshti.dandun.base.user.entity.ChatEntity;
 import lombok.Data;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Data
 public class ChatOutputDto {
     private Integer chatId;
     private Integer patientId;
     private Integer doctorId;
-    private List<MessageOutputDto> messageOutputDtoList;
+    private String lastMessage;
 
     public static ChatOutputDto fromEntity(ChatEntity entity) {
         ChatOutputDto dto = new ChatOutputDto();
         dto.setChatId(entity.getChatId());
         dto.setDoctorId(entity.getDoctorId());
         dto.setPatientId(entity.getPatientId());
-        dto.setMessageOutputDtoList(entity
-                .getMessageEntityList()
-                .stream()
-                .map(MessageOutputDto::fromEntity)
-                .collect(Collectors.toList()));
+        dto.setLastMessage(entity.getMessageEntityList().get(entity.getMessageEntityList().size() - 1).getMessage());
         return dto;
     }
 }
